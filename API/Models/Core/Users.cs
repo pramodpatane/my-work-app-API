dc6f5dcb2178a54eea4dbe6d0049066d7daab4d4
pramodpatane <pramodpatane9690@gmail.com>
2026-02-25 10:58:07 +0530

    Update Users.cs

--- API/Models/Core/Users.cs
+++ API/Models/Core/Users.cs
@@ -1,22 +1,18 @@
 ﻿
 namespace API.Models.Core
 {
-    public class Users
+    public class Users: BaseEntity
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }    
+        public string? Password { get; set; }    
         public byte[]? PasswordHash { get; set; }
         public byte[]? PasswordSalt { get; set; }
         public string? Phone { get; set; }
         public int RoleId { get; set; }        
         public string Role { get; set; }
         public string ProfileImageURL { get; set; }
-        public bool IsEmailVerified { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsDeleted { get; set; }
-        public string CreatedBy { get; set; }    
+        public bool IsEmailVerified { get; set; }           
     }
 }
